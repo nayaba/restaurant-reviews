18bad26807e38205e682133c65537daaea751d3b
Nabila Ayaba <nabila.ayaba@gmail.com>
2022-08-01 13:50:48 -0400

    Refactor the way controllers are imported in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
 const { Router } = require('express')
 const router = Router()
-const { getAll, createOne, findOne } = require('./../controllers/restaurants')
+const restaurant = require('./../controllers/restaurants')
+const review = require('./../controllers/reviews')
 
 // restaurant routes
-router.get('/restaurants', getAll)
-router.post('/restaurants', createOne)
-router.get('/restaurants/:id', findOne)
+router.get('/restaurants', restaurant.getAll)
+router.post('/restaurants', restaurant.createOne)
+router.get('/restaurants/:id', restaurant.findOne)
 
 // review routes
-router.get('/reviews', (req, res) => {
-  res.send('this route works!')
-})
+router.get('/reviews', review.getAll)
 
 module.exports = router
